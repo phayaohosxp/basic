b897112ccb941e304bb79b11ccf42a5eb76aa2f1
phayaohosxp <phayaodatacenter@gmail.com>
2016-04-28 00:15:08 +0700

    Delete db.php

--- config/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=203.157.102.156;dbname=hdc',
-    'username' => 'xxx',
-    'password' => 'xxxx',
-    'charset' => 'utf8',
-];
